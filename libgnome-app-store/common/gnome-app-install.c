b6407a0aac84671d1d9d76fefaad309cf925073a
David Liang <liangchenye@gmail.com>
2011-10-12 11:33:37 +0800

    donnot call install

--- libgnome-app-store/common/gnome-app-install.c
+++ libgnome-app-store/common/gnome-app-install.c
@@ -5,6 +5,9 @@
 gboolean
 gnome_app_install (gchar *pkgname)
 {
+	/* FIXME: TODO */
+	return TRUE;
+
         gchar *cmd;
         cmd = g_strdup_printf ("/sbin/YaST2 -i %s &", pkgname);
 	printf ("run cmd [%s]\n", cmd);
