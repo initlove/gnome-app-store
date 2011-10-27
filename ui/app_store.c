c74e7c216b17e9bfcf00e805b3c5ad2f0a684b4b
David Liang <liangchenye@gmail.com>
2011-10-27 15:59:51 +0800

    test on the script

--- ui/app_store.c
+++ ui/app_store.c
@@ -6,21 +6,53 @@
 
 #include "gnome-app-config.h"
 #include "gnome-app-store.h"
-
+#include "gnome-app-ui-score.h"
 #include "gnome-app-stage.h"
 #include "gnome-app-info-ui.h"
 
 #define TEST_SERVER FALSE
 #define TEST_GAME FALSE
 
+static ClutterScript *script = NULL;
+static guint merge_id = 0;
+
+static gboolean
+blue_button_press (ClutterActor       *actor,
+                   ClutterButtonEvent *event,
+                   gpointer            data)
+{
+  g_print ("[*] Pressed '%s'\n", clutter_get_script_id (G_OBJECT (actor)));
+  g_print ("[*] Unmerging objects with merge id: %d\n", merge_id);
+
+  clutter_script_unmerge_objects (script, merge_id);
+
+  return TRUE;
+}
+
+static gboolean
+red_button_press (ClutterActor *actor,
+                  ClutterButtonEvent *event,
+                  gpointer            data)
+{
+  GObject *timeline;
+
+  g_print ("[*] Pressed '%s'\n", clutter_get_script_id (G_OBJECT (actor)));
+
+  timeline = clutter_script_get_object (script, "main-timeline");
+  g_assert (CLUTTER_IS_TIMELINE (timeline));
+
+  if (!clutter_timeline_is_playing (CLUTTER_TIMELINE (timeline)))
+    clutter_timeline_start (CLUTTER_TIMELINE (timeline));
+  else
+    clutter_timeline_pause (CLUTTER_TIMELINE (timeline));
+
+  return TRUE;
+}
+
 /* remove all the contents . */
 int
 main (int argc, char *argv[])
 {
-	ClutterActor *stage;
-	ClutterActor *actor;
-	GnomeAppConfig *conf;
-	ClutterScript *script;
 	gchar *filename;
 
 	if (clutter_init (&argc, &argv) != CLUTTER_INIT_SUCCESS)
@@ -29,12 +61,70 @@ main (int argc, char *argv[])
 	if (argc > 1)
 		filename = argv [1];
 	else
-		filename = "/home/novell/gnome-app-store/ui/scripts/test-script.json";
+		filename = "/home/novell/gnome-app-store/ui/scripts/page-info.json";
+
+	ClutterActor *stage, *actor;
+	GnomeAppInfo *info;
+        GnomeAppQuery *query;
+	GnomeAppStore *store;
+        GList *l, *list;
+	gint i;
+
+        g_type_init ();
+
+	store = gnome_app_store_get_default ();
+        query = gnome_app_query_new ();
+        g_object_set (query, QUERY_PAGESIZE, "35", QUERY_PAGE, "0", NULL);
+        list = gnome_app_store_get_apps_by_query (store, query);
+	l = list;
+	l = l->next;
+	info = l->data;
 
 	script = clutter_script_new ();
 	clutter_script_load_from_file (script, filename, NULL);
 	clutter_script_get_objects (script, "main-stage", &stage, NULL);
 
+	gchar *prop [] = {
+		"name", "personid", "description", 
+		"score",
+		"smallpreviewpic1", "previewpic1", 
+		"license", NULL};
+
+	const gchar *val;
+	gchar *local_uri;
+
+//	gnome_app_info_debug (info);
+	for (i = 0; prop [i]; i++) {
+		clutter_script_get_objects (script, prop [i], &actor, NULL);
+		if (!actor)
+			continue;
+		val = gnome_app_info_get (info, prop [i]);
+		if (CLUTTER_IS_TEXTURE (actor)) {
+			local_uri = gnome_app_get_local_icon (val);
+			clutter_texture_set_from_file (actor, local_uri, NULL);
+			g_free (local_uri);
+		} else if (CLUTTER_IS_TEXT (actor)) {
+			clutter_text_set_text (actor, val);
+		} 
+#if 0
+/*FIXME: cannot use the user defined object? */
+			else if (GNOME_APP_IS_UI_SCORE (actor)) {
+			gnome_app_ui_score_set_score (actor, val);
+		}
+#endif
+	}
+
+	gchar *scores [] = { "score-1", "score-2", "score-3", "score-4", "score-5", NULL};
+	gint app_score = atoi (gnome_app_info_get (info, "score")) / 20;
+	for (i = 0; scores [i]; i++) {
+		clutter_script_get_objects (script, scores [i], &actor, NULL);
+		if (!actor)
+			continue;
+		if (i < app_score)
+			clutter_texture_set_from_file (actor, "/home/novell/gnome-app-store/pixmaps/starred.png", NULL);
+		else
+			clutter_texture_set_from_file (actor, "/home/novell/gnome-app-store/pixmaps/non-starred.png", NULL);
+	}
         clutter_actor_show (stage);
 	clutter_main ();
 
---
+++ ui/gnome-app-ui-score.c
@@ -0,0 +1,104 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*-
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+Author: liangchenye <liangchenye@gmail.com>
+
+*/
+#include <gio/gio.h>
+#include <gdesktop-enums.h>
+#include <string.h>
+
+#include <clutter/clutter.h>
+
+#include "gnome-app-utils.h"
+#include "gnome-app-ui-score.h"
+
+struct _GnomeAppUIScorePrivate
+{
+	gint score;
+};
+
+G_DEFINE_TYPE (GnomeAppUIScore, gnome_app_ui_score, CLUTTER_TYPE_GROUP)
+
+static void
+ui_score_load (GnomeAppUIScore *ui)
+{
+	ClutterActor *layout;
+	ClutterActor *actor;
+
+	layout = clutter_box_new (clutter_box_layout_new ());
+	clutter_container_add_actor (CLUTTER_CONTAINER (ui), layout);
+
+	gint i, score;
+	
+	score = ui->priv->score / 20;
+	for (i = 0; i < score; i++) {
+		actor = clutter_texture_new_from_file ("/home/novell/gnome-app-store/pixmaps/starred.png", NULL);
+	        clutter_container_add_actor (CLUTTER_CONTAINER (layout), actor);
+	}
+	for (; i < 5; i++) {
+		actor = clutter_texture_new_from_file ("/home/novell/gnome-app-store/pixmaps/non-starred.png", NULL);
+	        clutter_container_add_actor (CLUTTER_CONTAINER (layout), actor);
+	}
+}
+
+static void
+gnome_app_ui_score_init (GnomeAppUIScore *ui)
+{
+	GnomeAppUIScorePrivate *priv;
+
+	ui->priv = priv = G_TYPE_INSTANCE_GET_PRIVATE (ui,
+							 GNOME_APP_TYPE_UI_SCORE,
+							 GnomeAppUIScorePrivate);
+	priv->score = 0;
+	ui_score_load (ui);
+}
+
+static void
+gnome_app_ui_score_dispose (GObject *object)
+{
+	G_OBJECT_CLASS (gnome_app_ui_score_parent_class)->dispose (object);
+}
+
+static void
+gnome_app_ui_score_finalize (GObject *object)
+{
+	GnomeAppUIScore *ui = GNOME_APP_UI_SCORE (object);
+	GnomeAppUIScorePrivate *priv = ui->priv;
+
+	G_OBJECT_CLASS (gnome_app_ui_score_parent_class)->finalize (object);
+}
+
+static void
+gnome_app_ui_score_class_init (GnomeAppUIScoreClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	object_class->dispose = gnome_app_ui_score_dispose;
+	object_class->finalize = gnome_app_ui_score_finalize;
+	 
+	g_type_class_add_private (object_class, sizeof (GnomeAppUIScorePrivate));
+}
+
+GnomeAppUIScore *
+gnome_app_ui_score_new (void)
+{
+	return g_object_new (GNOME_APP_TYPE_UI_SCORE, NULL);
+}
+
+void
+gnome_app_ui_score_set_score (GnomeAppUIScore *ui, gchar *score)
+{
+	if (score)
+		ui->priv->score = atoi (score);
+
+	ui_score_load (ui);
+}
